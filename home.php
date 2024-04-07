6b39245f7318f80056f7c00b9a95f364c8d723bd
Thithiksha <thithiksha.slt@gmail.com>
2024-04-07 22:38:15 +0530

    images background

--- home.php
+++ home.php
@@ -11,6 +11,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="override.css">
+  <script type="text/javascript" src="js/changebg.js" async></script>
 </head>
 <body>
     <div class="content">
@@ -41,157 +42,6 @@
 		</div>
 	</div> <!--home ends-->
 
- <?php
-
-/*  echo '<!--upcoming section-->
-  <div class="sectionHeader" id="Upcoming">
-    <div class="header">
-      <div class="headingText">Upcoming Trips</div>
-      <div class="viewAllBtn"><button class="secondaryBtn" type="button" id="VABtn" data-toggle="collapse" data-target="#AllTrips" aria-expanded="false" aria-controls="AllTrips" onclick="javascript:toggleText();">View All</button></div>
-    </div>
-    <div id ="cards" class="row mx-auto">';
-
-    $servername = 'localhost';
-                    $username = 'root';
-                    $password = '';
-                    $db='letstravel';
-                    $conn = mysqli_connect($servername,$username,$password,$db);
-
-  $sql="SELECT * FROM trip WHERE Status=1";
-  $result = mysqli_query($conn,$sql);
-  $rows = mysqli_num_rows($result);
-  if($rows<=4)
-  {  $limit = $rows;}
-  else
-  {  $limit = 4;}
-  for($i=0;$i<$limit;$i++)
-  {
-    $tripID = mysqli_fetch_assoc($result);
-    $sql2='SELECT locations from trip_location where tripId="'.$tripID["TripId"].'"';
-    $result2 = mysqli_query($conn,$sql2);
-    $rows2 = mysqli_num_rows($result2);
-    $temp =  mysqli_fetch_assoc($result2);
-    $locs = $temp["locations"];
-    for($j=1;$j<$rows2;$j++)
-    {
-      $temp =  mysqli_fetch_assoc($result2);
-      $locs=$locs." - ".$temp["locations"];
-    }
-
-    echo 
-        '<div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="uploads/'.$tripID["Image"].'"Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">'.$locs.'</h5>
-                <p class="card-text">Starting from ₹'.$tripID["BasePrice"].'</p>
-                <form action="home.php" method="post">
-                 <a href="#" data-toggle="modal" data-target="#tripDetails'.$i.'" class="toggle" class="viewBtn"><input type="submit" name="trip_sel" value="View Details" class="viewBtn"> </a>
-                </form>
-              </div>
-          </div>
-        </div>'
-      ;
-
-      echo '<!--modal-->
-      <form action="join.php" method="post">
-      <input type="hidden" name="tripid" value="'.$tripID["TripId"].'">
-    <div class="modal fade" id="tripDetails'.$i.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">'.$locs.'</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body" id="carouselimg">
-                <img class="img-fluid" src="uploads/'.$tripID["Image"].'">
-                <div class="modalText">
-                  <label><b>Date:</b> '.$tripID["StartDate"].' to '.$tripID["EndDate"].'<br>
-                  <a href="uploads/'.$tripID["Itinerary"].'" download class="downloadFile">Download itinerary</a><br>
-                  <label><b>Price:</b> ₹'.$tripID["BasePrice"].'*</label>
-                  <p id="terms">* The charges mentioned above includes only the base price of the trip for the mentioned location, exclusive of accommodation and travel charges from your city. </p>
-              </div>
-           
-            </div>
-            <div class="modal-footer">
-              <input type="submit" class="yellowBtn joinBtn" name="tripjoined" value="Join Now"></form>
-            </div>
-        </div>
-      </div>
-  </div>
-    <!--modal ends-->'; 
-
-  }
-  if($rows>4)
-  {
-    echo '<div class="collapse" id="AllTrips">';
-    for($i=4;$i<$rows;$i++)
-  {
-    $tripID = mysqli_fetch_assoc($result);
-    $sql2='SELECT locations from trip_location where tripId="'.$tripID["TripId"].'";';
-    $result2 = mysqli_query($conn,$sql2);
-    $rows2 = mysqli_num_rows($result2);
-    $temp =  mysqli_fetch_assoc($result2);
-    $locs = $temp["locations"];
-    for($j=1;$j<$rows2;$j++)
-    {
-      $temp =  mysqli_fetch_assoc($result2);
-      $locs=$locs." - ".$temp["locations"];
-    }
-
-    echo 
-        '<div class="col-sm-3">
-          <div class="card" style="width: 18rem;">
-              <img class="card-img-top" src="uploads/'.$tripID["Image"].'"Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">'.$locs.'</h5>
-                <p class="card-text">Starting from ₹'.$tripID["BasePrice"].'</p>
-                <form action="home.php" method="post">
-                 <a href="#" data-toggle="modal" data-target="#tripDetails'.$i.'" class="toggle" class="viewBtn"><input type="submit" name="trip_sel" value="View Details" class="viewBtn"> </a>
-                </form>
-              </div>
-          </div>
-        </div>'
-      ;
-
-      echo '<!--modal-->
-       <form action="join.php" method="post">
-      <input type="hidden" name="tripid" value="'.$tripID["TripId"].'">
-    <div class="modal fade" id="tripDetails'.$i.'" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">'.$locs.'</h5>
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-              </button>
-            </div>
-            <div class="modal-body" id="carouselimg">
-                <img class="img-fluid" src="uploads/'.$tripID["Image"].'">
-                <div class="modalText">
-                  <label><b>Date:</b> '.$tripID["StartDate"].' to '.$tripID["EndDate"].'<br>
-                  <a href="uploads/'.$tripID["Itinerary"].'" download class="downloadFile">Download itinerary</a><br>
-                  <label><b>Price:</b> ₹'.$tripID["BasePrice"].'*</label>
-                  <p id="terms">* The charges mentioned above includes only the base price of the trip for the mentioned location, exclusive of accommodation and travel charges from your city. </p>
-              </div>
-           
-            </div>
-            <div class="modal-footer">
-               <input type="submit" class="yellowBtn joinBtn" value="Join Now" name="tripjoined"></form>
-            </div>
-        </div>
-      </div>
-  </div>
-    <!--modal ends-->'; 
-
-  }
-      echo '</div>';
-  }
-
-  echo  '</div></div>';*/
-  ?> 
   
     <!--footer-->
     	<footer class="page-footer font-small pt-4">
@@ -224,52 +74,7 @@
     		Made by<font style="color: #ffc312;"> Grusha Dharod, Vicky Daiya & Shivanee Jaiswal</font>
     	</div>
   	</footer>
-<!--Bootstrap JS-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<script type="text/javascript">
 
-  function toggleText()
-  {
-    var but = document.getElementById("VABtn");
-    if(but.innerHTML=="View All")
-      but.innerHTML="Hide";
-    else if(but.innerHTML=="Hide")
-      but.innerHTML="View All";
-  }
-
-  // onload document window using js
-window.onload = function(){
-    //   pick body element ID
-    var image = document.getElementById("content");
-      
-    //   Background Images
-      var images = [
-          'url(images/slideshow/img1.jpg)',
-          'url(images/slideshow/img2.jpg)',
-          'url(images/slideshow/img3.jpg)',
-          'url(images/slideshow/img4.jpg)',
-          'url(images/slideshow/img5.jpg)',
-          'url(images/slideshow/img6.jpg)',
-          'url(images/slideshow/img7.jpg)',
-          
-      ]
-      console.log("Image element:", image);
-
-    //   change image every after 5 seconds
-      var i = 0;
-      i = Math.floor(images.length * Math.random());
-      console.log(i);
-      image.style.backgroundImage = 'linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), ' + images[i];
-      image.style.backgroundSize = "cover";
-      image.style.backgroundRepeat = "no-repeat";
-      image.style.backgroundPosition = "center";
-
-}
-</script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/animations.js"></script>
 </body>
 </html>
 
---
+++ js/changebg.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var image = document.querySelector(".content");
+
+    var images = [
+        'url(images/slideshow/img1.jpg)',
+        'url(images/slideshow/img2.jpg)',
+        'url(images/slideshow/img3.jpg)',
+        'url(images/slideshow/img4.jpg)',
+        'url(images/slideshow/img5.jpg)',
+        'url(images/slideshow/img6.jpg)',
+        'url(images/slideshow/img7.jpg)'
+    ];
+
+    console.log("Image element:", image);
+
+    var i = Math.floor(images.length * Math.random());
+    console.log(i);
+
+    image.style.backgroundImage = images[i];
+    image.style.backgroundSize = "cover";
+    image.style.backgroundRepeat = "no-repeat";
+    image.style.backgroundPosition = "center";
+});
