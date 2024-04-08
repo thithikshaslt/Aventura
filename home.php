dbe11fc2276c8b8ac13ae9f9ca2bca139175d648
Thithiksha <thithiksha.slt@gmail.com>
2024-04-08 22:19:32 +0530

    bootstrap modal update

--- home.php
+++ home.php
@@ -16,6 +16,7 @@
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
 
 <!-- Then include bootstrap js -->
+
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
   <script type="text/javascript" src="js/changebg.js" async></script>
